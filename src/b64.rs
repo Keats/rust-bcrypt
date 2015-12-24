baa02dc64f755ed27a55d267e41ff65f1862fdb9
Vincent Prouillet <vincent@wearewizards.io>
2015-12-24 21:22:26 +0000

    Working ?

---
+++ src/b64.rs
@@ -0,0 +1,190 @@
+use std::collections::HashMap;
+
+use rustc_serialize::base64::{self, FromBase64, ToBase64};
+
+
+// Decoding table from bcrypt base64 to standard base64 and standard -> bcrypt
+// Bcrypt has its own base64 alphabet
+// ./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+lazy_static! {
+    static ref BCRYPT_TO_STANDARD: HashMap<char, char> = {
+        let mut m = HashMap::new();
+        m.insert('/', 'B');
+        m.insert('.', 'A');
+        m.insert('1', '3');
+        m.insert('0', '2');
+        m.insert('3', '5');
+        m.insert('2', '4');
+        m.insert('5', '7');
+        m.insert('4', '6');
+        m.insert('7', '9');
+        m.insert('6', '8');
+        m.insert('9', '/');
+        m.insert('8', '+');
+        m.insert('A', 'C');
+        m.insert('C', 'E');
+        m.insert('B', 'D');
+        m.insert('E', 'G');
+        m.insert('D', 'F');
+        m.insert('G', 'I');
+        m.insert('F', 'H');
+        m.insert('I', 'K');
+        m.insert('H', 'J');
+        m.insert('K', 'M');
+        m.insert('J', 'L');
+        m.insert('M', 'O');
+        m.insert('L', 'N');
+        m.insert('O', 'Q');
+        m.insert('N', 'P');
+        m.insert('Q', 'S');
+        m.insert('P', 'R');
+        m.insert('S', 'U');
+        m.insert('R', 'T');
+        m.insert('U', 'W');
+        m.insert('T', 'V');
+        m.insert('W', 'Y');
+        m.insert('V', 'X');
+        m.insert('Y', 'a');
+        m.insert('X', 'Z');
+        m.insert('Z', 'b');
+        m.insert('a', 'c');
+        m.insert('c', 'e');
+        m.insert('b', 'd');
+        m.insert('e', 'g');
+        m.insert('d', 'f');
+        m.insert('g', 'i');
+        m.insert('f', 'h');
+        m.insert('i', 'k');
+        m.insert('h', 'j');
+        m.insert('k', 'm');
+        m.insert('j', 'l');
+        m.insert('m', 'o');
+        m.insert('l', 'n');
+        m.insert('o', 'q');
+        m.insert('n', 'p');
+        m.insert('q', 's');
+        m.insert('p', 'r');
+        m.insert('s', 'u');
+        m.insert('r', 't');
+        m.insert('u', 'w');
+        m.insert('t', 'v');
+        m.insert('w', 'y');
+        m.insert('v', 'x');
+        m.insert('y', '0');
+        m.insert('x', 'z');
+        m.insert('z', '1');
+        m
+    };
+    static ref STANDARD_TO_BCRYPT: HashMap<char, char> = {
+        let mut m = HashMap::new();
+        m.insert('B', '/');
+        m.insert('A', '.');
+        m.insert('3', '1');
+        m.insert('2', '0');
+        m.insert('5', '3');
+        m.insert('4', '2');
+        m.insert('7', '5');
+        m.insert('6', '4');
+        m.insert('9', '7');
+        m.insert('8', '6');
+        m.insert('/', '9');
+        m.insert('+', '8');
+        m.insert('C', 'A');
+        m.insert('E', 'C');
+        m.insert('D', 'B');
+        m.insert('G', 'E');
+        m.insert('F', 'D');
+        m.insert('I', 'G');
+        m.insert('H', 'F');
+        m.insert('K', 'I');
+        m.insert('J', 'H');
+        m.insert('M', 'K');
+        m.insert('L', 'J');
+        m.insert('O', 'M');
+        m.insert('N', 'L');
+        m.insert('Q', 'O');
+        m.insert('P', 'N');
+        m.insert('S', 'Q');
+        m.insert('R', 'P');
+        m.insert('U', 'S');
+        m.insert('T', 'R');
+        m.insert('W', 'U');
+        m.insert('V', 'T');
+        m.insert('Y', 'W');
+        m.insert('X', 'V');
+        m.insert('a', 'Y');
+        m.insert('Z', 'X');
+        m.insert('b', 'Z');
+        m.insert('c', 'a');
+        m.insert('e', 'c');
+        m.insert('d', 'b');
+        m.insert('g', 'e');
+        m.insert('f', 'd');
+        m.insert('i', 'g');
+        m.insert('h', 'f');
+        m.insert('k', 'i');
+        m.insert('j', 'h');
+        m.insert('m', 'k');
+        m.insert('l', 'j');
+        m.insert('o', 'm');
+        m.insert('n', 'l');
+        m.insert('q', 'o');
+        m.insert('p', 'n');
+        m.insert('s', 'q');
+        m.insert('r', 'p');
+        m.insert('u', 's');
+        m.insert('t', 'r');
+        m.insert('w', 'u');
+        m.insert('v', 't');
+        m.insert('y', 'w');
+        m.insert('x', 'v');
+        m.insert('0', 'y');
+        m.insert('z', 'x');
+        m.insert('1', 'z');
+        m.insert('=', '=');
+        m
+    };
+}
+
+pub fn encode(words: &[u8]) -> String {
+    let hash = words.to_base64(base64::STANDARD);
+    let mut vec: Vec<String> = Vec::with_capacity(hash.len());
+    for ch in hash.chars() {
+        vec.push(STANDARD_TO_BCRYPT.get(&ch).unwrap().clone().to_string());
+    }
+    vec.concat().replace("=", "")
+}
+
+// Can potentially panic if the hash given contains invalid characters
+pub fn decode(hash: &str) -> Vec<u8> {
+    let mut vec: Vec<String> = Vec::with_capacity(hash.len());
+    for ch in hash.chars() {
+        vec.push(BCRYPT_TO_STANDARD.get(&ch).unwrap().clone().to_string());
+    }
+    // Bcrypt base64 has no padding but standard has
+    // so we need to actually add padding ourselves
+    if hash.len() % 4 > 0 {
+        let padding = 4 - hash.len() % 4;
+        for _ in 0..padding {
+            vec.push("=".to_owned());
+        }
+    }
+    vec.concat().from_base64().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{decode, encode};
+
+    #[test]
+    fn can_decode_bcrypt_base64() {
+        let hash = "YETqZE6eb07wZEO";
+        assert_eq!("hello world", String::from_utf8_lossy(&decode(hash)));
+    }
+
+    #[test]
+    fn can_encode_to_bcrypt_base64() {
+        let expected = "YETqZE6eb07wZEO";
+        assert_eq!(encode("hello world".as_bytes()), expected);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 #![cfg_attr(feature = "dev", feature(plugin))]
 #![cfg_attr(feature = "dev", plugin(clippy))]
 
+#[macro_use]
+extern crate lazy_static;
 extern crate crypto;
 extern crate rand;
 extern crate rustc_serialize;
@@ -14,9 +16,9 @@ use std::io;
 use crypto::bcrypt::bcrypt;
 use crypto::util::fixed_time_eq;
 use rand::{Rng, OsRng};
-use rustc_serialize::base64::{self, ToBase64, FromBase64};
 
-mod base64;
+mod b64;
+
 
 // Cost constants
 static MIN_COST: u32 = 4;
@@ -34,20 +36,8 @@ struct HashParts {
 impl HashParts {
     fn format(self) -> String {
         // Cost need to have a length of 2 so padding with a 0 if cost < 10
-        let cost_str = if self.cost < 10 {
-            format!("0{}", self.cost)
-        } else {
-            format!("{}", self.cost)
-        };
-
-        format!("$2y${}${}{}", cost_str, self.salt, self.hash)
+        format!("$2y${:02}${}{}", self.cost, self.salt, self.hash)
     }
-}
-
-fn null_terminate(password: &str) -> Vec<u8> {
-    let mut v: Vec<_> = password.bytes().collect();
-    v.push(0);
-    v
 }
 
 fn _hash(password: &str, cost: u32, salt: &[u8]) -> HashParts {
@@ -55,15 +45,22 @@ fn _hash(password: &str, cost: u32, salt: &[u8]) -> HashParts {
     let mut output = [0u8; 24];
     // We only consider the first 72 chars
     let password_bytes: &[u8] = password.as_ref();
-    let pass = if password_bytes.len() > 72 { &password_bytes[..72] } else { password_bytes };
-    bcrypt(cost, &salt, pass, &mut output);
+    let pass = if password_bytes.len() > 72 {
+        &password_bytes[..72]
+    } else {
+        password_bytes
+    };
+    // Passwords need to be null terminated
+    let mut vec = Vec::new();
+    vec.extend(pass);
+    vec.push(0);
 
-    println!("{:?}", salt.to_base64(base64::BCRYPT));
-    println!("{:?}", salt.to_base64(base64::STANDARD));
+    bcrypt(cost, &salt, &vec, &mut output);
+
     HashParts {
         cost: cost,
-        salt: salt.to_base64(base64::BCRYPT),
-        hash: output.to_base64(base64::BCRYPT)
+        salt: b64::encode(&salt),
+        hash: b64::encode(&output[..23])
     }
 }
 
@@ -103,6 +100,7 @@ fn split_hash(hash: &str) -> HashParts {
                 }
             },
             3 => {
+                println!("{:?}, {}", part, part.len());
                 if part.len() == 53 {
                     parts.salt = part[..22].chars().collect();
                     parts.hash = part[22..].chars().collect();
@@ -119,15 +117,13 @@ fn split_hash(hash: &str) -> HashParts {
 pub fn verify(password: &str, hash: &str) -> Result<bool, ()> {
     let parts = split_hash(hash);
     println!("{:?}", parts);
-    let salt = parts.salt.from_base64().unwrap();
+    // TODO: handle invalid hash parts
+    let salt = b64::decode(&parts.salt);
+    let generated = _hash(password, parts.cost, &salt);
+    println!("{:?}", generated);
 
-    let generated = _hash(
-        &String::from_utf8(null_terminate(password)).unwrap(),
-        parts.cost,
-        &salt
-    );
     // Hashes should be the same given same salt and round number
-    Ok(fixed_time_eq(parts.hash.as_ref(), generated.hash.as_ref()))
+    Ok(fixed_time_eq(&b64::decode(&parts.hash), &b64::decode(&generated.hash)))
 }
 
 
@@ -150,12 +146,30 @@ mod tests {
     #[test]
     fn can_verify_hash_generated_from_some_online_tool() {
         let hash = "$2a$04$UuTkLRZZ6QofpDOlMz32MuuxEHA43WOemOYHPz6.SjsVsyO1tDU96";
-        assert_eq!(true, verify("password", hash).unwrap());
+        assert!(verify("password", hash).unwrap() == true);
     }
 
-    // #[test]
-    // fn can_verify_own_generated() {
-    //     let hashed = hash("hunter2", 4).unwrap();
-    //     assert_eq!(true, verify("hunter2", &hashed));
-    // }
+    #[test]
+    fn can_verify_hash_generated_from_python() {
+        let hash = "$2b$04$EGdrhbKUv8Oc9vGiXX0HQOxSg445d458Muh7DAHskb6QbtCvdxcie";
+        assert!(verify("correctbatteryhorsestapler", hash).unwrap() == true);
+    }
+
+    #[test]
+    fn can_verify_hash_generated_from_node() {
+        let hash = "$2a$04$n4Uy0eSnMfvnESYL.bLwuuj0U/ETSsoTpRT9GVk5bektyVVa5xnIi";
+        assert!(verify("correctbatteryhorsestapler", hash).unwrap() == true);
+    }
+
+    #[test]
+    fn a_wrong_password_is_false() {
+        let hash = "$2b$04$EGdrhbKUv8Oc9vGiXX0HQOxSg445d458Muh7DAHskb6QbtCvdxcie";
+        assert!(verify("wrong", hash).unwrap() == false);
+    }
+
+    #[test]
+    fn can_verify_own_generated() {
+        let hashed = hash("hunter2", 4).unwrap();
+        assert_eq!(true, verify("hunter2", &hashed).unwrap());
+    }
 }
